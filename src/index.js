27a419dd460767a25b8fad722e3e59a3507ea0d8
kkunterr <kunterpartel@gmail.com>
2019-05-14 19:41:44 +0300

    NPM init && file setup

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const expresss = require('express');
+const chat = express();
+const port = process.env.PORT || 3000;
+const publicDirectory = path.join(__dirname, '../public');
+
+
+
+
+chat.use(express.static(publicDirectory));
+chat.listen(port, () => {
+    console.log(`Server jookseb port ${port} peal`);
+});
+
+
+
+
+
