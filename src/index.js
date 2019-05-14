b271935220d0406751267dcb617a1e28e60d313f
kkunterr <kunterpartel@gmail.com>
2019-05-14 20:06:49 +0300

    socket.io

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const path = require('path');
+const http = require('http');
+const socket = require('socket.io');
 const express = require('express');
 const chat = express();
+
+const server = http.createServer(chat);
+const io = socket(server);
 const port = process.env.PORT || 3000;
 const publicDirectory = path.join(__dirname, '../public');
 
 chat.use(express.static(publicDirectory));
-chat.listen(port, () => {
+
+io.on('connection', () => {
+    console.log('Uus klient..');
+});
+server.listen(port, () => {
     console.log(`Server jookseb port ${port} peal`);
 });
 
