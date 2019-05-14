845e233500ee7484a8fc77cc9f4fedc2d69b9c2a
kkunterr <kunterpartel@gmail.com>
2019-05-14 19:52:53 +0300

    css file, start script

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ const chat = express();
 const port = process.env.PORT || 3000;
 const publicDirectory = path.join(__dirname, '../public');
 
-
-
-
 chat.use(express.static(publicDirectory));
 chat.listen(port, () => {
     console.log(`Server jookseb port ${port} peal`);
